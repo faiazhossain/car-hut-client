12a7aa3a9da0ba88b87f53e534cdd2d5e8df8d8a
Faiaz Hossain <faiazhossain10@gmail.com>
2022-11-24 12:44:15 +0600

    navbar added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CustomersOpinion from "../CustomersOpinion/CustomersOpinion";
+import Banner from "./Banner";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Banner></Banner>
+      <CustomersOpinion></CustomersOpinion> */}
+    </div>
+  );
+};
+
+export default Home;
